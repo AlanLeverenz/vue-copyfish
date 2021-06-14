9cc0e0ca593217a888399dbbf71b6ef773a8c3e0
Alan Leverenz <awleverenz@aol.com>
2021-06-13 23:06:58 -0400

    opened first component - ScreenCapture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ScreenCapture />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ScreenCapture from '@/components/ScreenCapture.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ScreenCapture
   }
 }
 </script>
